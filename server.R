d3d2891a111cdf9c5bfa38606ee08915facc104e
hutchisonjtw <james.hutchison@jncc.gov.uk>
2019-10-15 22:45:28 +0100

    Removed code now in global.r plus various tidying of obsolete elements, and added leaflet map code for maps tab.

--- server.R
+++ server.R
@@ -1,46 +1,23 @@
-library(shiny)
-library(shinydashboard)
+
+#source("./global.R")
 
 shinyServer(function(input, output) {
     
-    library(raster)
-    library(RColorBrewer)
-    
-    brushBox <- reactiveValues(crop = NULL, update = NULL)
+    brushBox <- reactiveValues(zoom = extent(habMap), update = NULL)
     
     boundary <- reactiveValues(box = NULL, cells =  NULL)
     
-    boundary2 <- reactiveValues(topLeft = NULL, bottomRight = NULL)
+    #boundary2 <- reactiveValues(topLeft = NULL, bottomRight = NULL)
+
+    habMapPlot <- reactiveValues(map = habMap)
     
-    habMap <- raster("D:/St Helena/BBN input layers/LandcoverType.tif")
-    
-    habitats <- data.frame(ID = 1:10, habitat = c("Native forest", "Introduced forest", "Native shrubland", 
-                                                  "Introduced shrubland", "Flax", "Arable", "Pasture", 
-                                                  "Plantations", "Rural gardens", "Other"))
-    habitats$cols <- rev(brewer.pal(10, "Paired"))
-    
-    createRat <- function(map, habitats) {
-        mapFactor <- ratify(map)
-        mapRat <- data.frame(ID = levels(mapFactor)[[1]])
-        mapRat$habitat <- habitats$habitat[match(mapRat$ID, habitats$ID)]
-        mapRat$cols <- habitats$cols[match(mapRat$ID, habitats$ID)]
-        levels(mapFactor) <- mapRat
-        return(mapFactor)
-    }
-    
-    
-    habMapRat <- createRat(habMap, habitats)
-    
-    habMapPlot <- reactiveValues(map = habMapRat)
-    
-    habMapMat <- reactive(as.matrix(habMapPlot$map))
+    #habMapMat <- reactive(as.matrix(habMapPlot$map))
     
     
 ## Plot map, with zoom if boundary drawn - tried re-writing this with rasterVis::levelplot, but it uses lattice graphics and the plotOutput brush function does not return the plot coordinates with lattice graphics so cannot be used to interact with the raster.    
     output$habMap <- renderPlot({
         par(mar = c(4,4,2,15))
-        plot(habMapPlot$map, col = levels(habMapPlot$map)[[1]]$cols, legend = FALSE)
-        legend("right", legend = levels(habMapPlot$map)[[1]]$habitat, fill = levels(habMapPlot$map)[[1]]$cols, xpd = TRUE, inset = -0.25, bty = "n")
+        zoomRatPlot(habMapPlot$map, brushBox$zoom)
     })
     
     # output$clickCoords <- renderText({
@@ -52,38 +29,33 @@ shinyServer(function(input, output) {
     observeEvent(input$habMap_dblclick, {
         brush <- input$habMap_brush
         if (!is.null(brush)) {
-            plotZoom$x <- extent(c(brush$xmin, brush$xmax, brush$ymin, brush$ymax))
-            croppedMap <- crop(habMap, plotZoom$x)
-            croppedMapRat <- createRat(croppedMap, habitats)
-            habMapPlot$map <- croppedMapRat
+            brushBox$zoom <- extent(c(brush$xmin, brush$xmax, brush$ymin, brush$ymax))
         } else {
-            plotZoom$x <- NULL
-            habMapPlot$map <- habMapRat
+            brushBox$zoom <- extent(habMap)
         }
     })
     
-    observeEvent(input$habMap_click, {
-        brush <- input$habMap_brush
-        if (!is.null(brush)) {
-            boundary$box <- extent(c(brush$xmin, brush$xmax, brush$ymin, brush$ymax))
-            boundary$cells <- cellsFromExtent(habMap, boundary$box)
-        } else {
-            boundary$box <- NULL
-            boundary$cells <- NULL
-        }
-    })
-    
-    output$boundaryBox <- renderText({
-        paste0("xmin = ", boundary$box@xmin,
-               "\nxmax = ", boundary$box@xmax,
-               "\nymin = ", boundary$box@ymin,
-               "\nymax = ", boundary$box@ymax,
-               "\nncells = ", length(boundary$cells),
-               "\nfromClass = ", input$fromClass,
-               "\ntoClass = ", input$toClass,
-               "\nboundary2 = ", boundary2$topLeft
-        )
-    })
+    # observeEvent(input$habMap_click, {
+    #     brush <- input$habMap_brush
+    #     if (!is.null(brush)) {
+    #         boundary$box <- extent(c(brush$xmin, brush$xmax, brush$ymin, brush$ymax))
+    #         boundary$cells <- cellsFromExtent(habMap, boundary$box)
+    #     } else {
+    #         boundary$box <- NULL
+    #         boundary$cells <- NULL
+    #     }
+    # })
+    # 
+    # output$boundaryBox <- renderText({
+    #     paste0("xmin = ", boundary$box@xmin,
+    #            "\nxmax = ", boundary$box@xmax,
+    #            "\nymin = ", boundary$box@ymin,
+    #            "\nymax = ", boundary$box@ymax,
+    #            "\nncells = ", length(boundary$cells),
+    #            "\nfromClass = ", input$fromClass,
+    #            "\ntoClass = ", input$toClass
+    #     )
+    # })
     
     # observeEvent(input$change, {
     #     brush <- input$habMap_brush
@@ -112,13 +84,12 @@ shinyServer(function(input, output) {
         brush <- input$habMap_brush
         if (!is.null(brush)) {
             brushBox$update <- extent(c(brush$xmin, brush$xmax, brush$ymin, brush$ymax))
-            habMapUpdate <- habMap
-            cells <- cellsFromExtent(habMapUpdate, plotZoom$x)
+            habMapUpdate <- habMapPlot$map
+            cells <- cellsFromExtent(habMapUpdate, brushBox$update)
             vals <- habMapUpdate[cells]
             vals[vals==input$fromClass] <- as.numeric(input$toClass)
             habMapUpdate[cells] <- vals
-            habMapUpdateRat <- createRat(habMapUpdate, habitats)
-            habMapPlot$map <- habMapUpdateRat
+            habMapPlot$map <- habMapUpdate
             #writeRaster(habMapPlot, file = "D:/St Helena/habmap.tif", overwrite = TRUE)
         } else {
             showModal(modalDialog(
@@ -126,5 +97,17 @@ shinyServer(function(input, output) {
                 easyClose = TRUE))
         }
     })
+    
+    observeEvent(input$btnReset, {
+        habMapPlot$map <- habMap
+    })
+    
+    output$serviceMap <- renderLeaflet({
+        leaflet() %>%
+            fitBounds(lng1 = -5.8045, lat1 = -16.04712, lng2 = -5.616973, lat2 = -15.8891) %>%
+            addProviderTiles(providers$Stamen.TonerLite) %>%
+            addRasterImage(as.factor(habMap), colors = rev(brewer.pal(10, "Paired")), group = "habMap") %>%
+            addRasterImage(habMapLatest, group = "habMapUpdate") %>%
+            addLayersControl(overlayGroups = c("habMap", "habMapUpdate"))
   
 })
